12064f48769d75e5a42b8ffd41b5b47df17de65c
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-15 15:32:03 +0530

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -11,21 +11,24 @@ extra_payment_end_month=int(input('enter end month:'))
 extra_payment=int(input('enter extra payment:'))
 while principal > 0:
     if months>=extra_payment_start_month and months<extra_payment_end_month:
-        principal = principal*(1+rate/12) - (payment+extra_payment)
-        total_paid = total_paid + payment+extra_payment
-        months+=1
-        if principal >=0:
-            print(months,total_paid,principal)
+        if principal<(payment+extra_payment):
+            total_paid = total_paid + principal
+            principal = 0
         else:
-            print(months,total_paid,0)
+            principal = principal * (1+rate/12) - (payment+extra_payment)
+            total_paid = total_paid + payment+extra_payment
+        months+=1
+        print(months,total_paid,principal)
+        
     else:
-        principal = principal*(1+rate/12) - payment 
-        total_paid = total_paid + payment
-        months+=1
-        if principal >=0:
-            print(months,total_paid,principal)
+        if principal<(payment+extra_payment):
+            total_paid = total_paid + principal
+            principal = 0
         else:
-            print(months,total_paid,0)
+            principal = principal * (1+rate/12) - payment
+            total_paid = total_paid + payment
+        months+=1
+        print(months,total_paid,principal)
      
 print('Total paid',total_paid)
 print('number of months req',months)
