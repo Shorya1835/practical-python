73b6e08ddab4dadfea394f5279d6db9ccf96adba
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-17 12:42:27 +0530

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
 # pcost.py
 #
 # Exercise 1.27
+with open('C:/Users/shory/OneDrive/Documents/GitHub/practical-pythons/Work/Data/portfolio.csv','rt') as f:
+    next(f)
+    for line in f:
+        pcost=line.split(',')
+        sum+=int(pcost[1])*float(pcost[2])
+    
+    print('Total cost',sum)
