bb435781edcc6ac124b35f41f131aba7a53b37eb
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-17 13:37:34 +0530

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,6 +2,7 @@
 #
 # Exercise 1.27
 import csv
+import sys
 def portfolio_cost(filename):
     sum=0.0
     with open(filename,'rt') as f:
@@ -13,6 +14,11 @@ def portfolio_cost(filename):
             except ValueError:
                 print("Couldn't parse",line,end='')
     return sum
-        
-cost=portfolio_cost('Data/portfolio.csv')
+    
+if len(sys.argv)==2:
+    filename=sys.argv[1]
+else:
+    filename='Data/portfolio.csv'
+
+cost=portfolio_cost(filename)
 print('Total cost:',cost)
