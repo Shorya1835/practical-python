c1b2c63b6d782d495c6f0b23dffdecc2c192ff88
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-17 15:18:08 +0530

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -3,10 +3,9 @@
 # Exercise 2.4
 import csv
 
-def portfolio_cost(filename):
+def read_portfolio(filename):
     portfolio=[]
-    
-
+   
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
@@ -14,3 +13,13 @@ def portfolio_cost(filename):
             holding={'name':row[0],'shares':int(row[1]),'price':float(row[2])}
             portfolio.append(holding)
     return portfolio
+def read_prices(filename):
+    prices=[]
+    
+    with open('Dataprices.csv','rt') as f:
+        row=csv.reader(f)
+        headers=next(rows)
+        for row in rows:
+            holding={'name':row[0],'price':row[1]}
+            prices.append(holding)
+    return prices
