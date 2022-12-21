50dea2d142db9143e53be4267046b89c9b55ce17
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-22 00:35:13 +0530

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -47,4 +47,8 @@ def make_report(portfolio,prices):
     print(f"{' ':->11s}{' ':->11s}{' ':->11s}{' ':->11s}")
     for s in portfolio:
         print(f"{s['name']:>10s} {s['shares']:>10d} {('$'+'{:0.2f}'.format(prices[s['name']])):>10s} {(prices[s['name']]-s['price']):>10.2f}")
-    
+
+def portfolio_report(fn1,fn2):
+    portfolio=read_portfolio(fn1)
+    prices=read_prices(fn2)
+    make_report(portfolio,prices)
