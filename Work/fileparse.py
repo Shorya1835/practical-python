625f7cb902d9bb01d448be1f59051355f713ef4d
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-24 07:18:55 +0530

    Update fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -2,7 +2,7 @@
 #
 # Exercise 3.3
 import csv
-def parse_csv(filename,select=[]):
+def parse_csv(filename,select=[],types=[]):
     '''
     Parse a CSV file into a list of records
     '''
@@ -14,11 +14,14 @@ def parse_csv(filename,select=[]):
             indices=[headers.index(colname) for colname in select]
         else:
             indices=[]
+            
         for row in rows:
             if not row:
                 continue
             if indices:
                 row=[row[index] for index in indices]
+            if types:
+                row=[func(val) for func,val in zip(types,row)]
             record=dict(zip(headers,row))
             records.append(record)
 
