9cd463f91af15767c404a4a100083fbe7092eacf
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-24 07:11:24 +0530

    Update fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -10,9 +10,15 @@ def parse_csv(filename):
         rows=csv.reader(f)
         headers=next(rows)
         records=[]
+        if select:
+            indices=[headers.index(colname) for colname in select]
+        else:
+            indices=[]
         for row in rows:
             if not row:
                 continue
+            if indices:
+                row=[row[index] for index in indices]
             record=dict(zip(headers,row))
             records.append(record)
 
