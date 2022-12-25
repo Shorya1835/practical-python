7bfb023196b9366f1b01683ab0278c90f27644ce
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-25 18:05:20 +0530

    Update stock.py

--- Work/stock.py
+++ Work/stock.py
@@ -1,5 +1,5 @@
 class stock:
-def _init_ (self,name,shares,price):
-    self.name=name
-    self.shares=int(shares)
-    self.price=float(prices)
+    def _init_ (self,name,shares,price):
+        self.name=name
+        self.shares=int(shares)
+        self.price=float(prices)
