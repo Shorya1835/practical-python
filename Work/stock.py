453b428806b9fc05a001d658f6468e60a0e5a582
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-25 18:21:49 +0530

    Update stock.py

--- Work/stock.py
+++ Work/stock.py
@@ -4,6 +4,6 @@ class Stock:
         self.shares=int(shares)
         self.price=float(price)
     def cost(self):
-        print(self.shares*self.price)
+        return self.shares*self.price
     def sell(self,x):
         self.shares=self.shares-x
