ea1596815d821d92a5e698b85b8088b352fe47d6
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2022-12-26 12:32:05 +0530

    Update tableformat.py

--- work/tableformat.py
+++ work/tableformat.py
@@ -1,12 +1,10 @@
 class TableFormatter:
   def headings(self,headers):
-      '''
-      Emit the table headings.
-      '''
-      raise NotImplementerError()
+      for h in headers:
+          print(f'{h:>10s}',end=' ')
+      print()
+      print(('-'*10 + ' ')*len(headers))
   def row(self,rowdata):
-    '''
-    Emit a single row of table data.
-    '''
-    raise NotImplementedError()
-    
+    for d in rowdata:
+        print(f'{d:>10s}',end=' ')
+    print()
