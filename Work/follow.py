4a14da11c3a5cf0f3799f4d5ed9e2be0ec9eeafe
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-17 03:47:51 +0530

    Update follow.py

--- Work/follow.py
+++ Work/follow.py
@@ -9,10 +9,10 @@ while True:
   if line=='':
     time.sleep(0.1)
     continue
-    fields=line.split(',')
-    name=fields[0].strip('"')
-    price=float(fields[1])
-    change=float(fields[4])
-    if change<0:
-        print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+  fields=line.split(',')
+  name=fields[0].strip('"')
+  price=float(fields[1])
+  change=float(fields[4])
+  if change<0:
+      print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
         
