0f610bb1349cb249da1b77ba57983a039f14cb7d
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-17 03:16:15 +0530

    Update portfolio.py

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -5,13 +5,13 @@ class Portfolio:
   def __iter__(self):
     return self.__holdings.__iter__()
     
-    @property
-    def total_cost(self):
-      return sum([s.cost for s in self._holdings])
+  @property
+  def total_cost(self):
+    return sum([s.cost for s in self._holdings])
     
-    def tabulate_shares(self):
-      from collections import Counter
-      total_shares=Counter()
-      for s in self._holdings:
-        total_shares[s.name]+=s.shares
-      return total_shares
+  def tabulate_shares(self):
+    from collections import Counter
+    total_shares=Counter()
+    for s in self._holdings:
+      total_shares[s.name]+=s.shares
+    return total_shares
