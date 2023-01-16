a121faa7bab28fdf4006f6c1f2e101b064ff30d2
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-17 03:05:33 +0530

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,14 +5,9 @@ import csv
 import sys
 import report
 def portfolio_cost(filename):
-    sum=0.0
     record=report.read_portfolio(filename)
-    for n,line in enumerate(record,start=1):
-        try:
-            sum+=int(line.shares)*float(line.price)
-        except ValueError:
-            print(f"Row {n}: Couldn't convert: {line}")    
-    return sum
+    for n,line in enumerate(record,start=1):    
+    return portfolio.total_cost
     
 if len(sys.argv)==2:
     filename=sys.argv[1]
