09963d31500c1af878f54becba53dc4bc0dddeb9
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-17 15:00:54 +0530

    Create ticker.py

---
+++ Work/ticker.py
@@ -0,0 +1,21 @@
+from follow import follow
+import csv
+
+def parse_stock_data(lines):
+    rows=csv.reader(lines)
+    return rows
+
+if __name__='__main__':
+    lines=follow('Data/stocklog.csv')
+    rows=parse_stock_data(lines)
+    for row in rows:
+        print(row)
+        
+def select_columns(rows,indices):
+    for row in rows:
+        yield [row[index] for index in indices]
+        
+def parse_stock_data(lines):
+    rows = csv.reader(lines)
+    rows=select_columns(rows,[0,1,4])
+    return rows
