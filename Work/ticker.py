45906fa79c96b23c05ab4dae2e3474d95e405dcc
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-17 15:05:14 +0530

    Update ticker.py

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,11 +1,6 @@
 from follow import follow
 import csv
 
-if __name__=='__main__':
-    lines=follow('Data/stocklog.csv')
-    rows=parse_stock_data(lines)
-    for row in rows:
-        print(row)
         
 def select_columns(rows,indices):
     for row in rows:
@@ -15,3 +10,9 @@ def parse_stock_data(lines):
     rows = csv.reader(lines)
     rows=select_columns(rows,[0,1,4])
     return rows
+
+if __name__=='__main__':
+    lines=follow('Data/stocklog.csv')
+    rows=parse_stock_data(lines)
+    for row in rows:
+        print(row)
