6e81b8f01231efdfb96b5edfa6d11ef079c68a9a
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-27 22:55:41 +0530

    Update timethis.py

--- Work/timethis.py
+++ Work/timethis.py
@@ -1,6 +1,10 @@
 import time
 def timethis(func):
-  start=time.time()
-  r=func(*args,**kwargs)
-  end=time.time()
-  print('%s.%s: %f' % (func.__module__, func.__name__, end-start))
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        try:
+            return func(*args,**kwargs)
+        finally:
+            end = time.time()
+            print("%s.%s : %f" % (func.__module__,func.__name__,end-start))
+    return wrapper
