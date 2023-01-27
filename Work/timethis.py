8decd406efd7ac07a3d11e0d3e4add50e17ee3b1
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-27 22:48:52 +0530

    Update timethis.py

--- Work/timethis.py
+++ Work/timethis.py
@@ -1,5 +1,6 @@
 import time
-start=time.time()
-r=func(*args,**kwargs)
-end=time.time()
-print('%s.%s: %f' % (func.__module__, func.__name__, end-start))
+def timethis(func):
+  start=time.time()
+  r=func(*args,**kwargs)
+  end=time.time()
+  print('%s.%s: %f' % (func.__module__, func.__name__, end-start))
