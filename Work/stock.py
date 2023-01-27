8e8edebe0eea2afa33d9248dda2a42d126b0cbe7
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-27 22:27:28 +0530

    Update stock.py

--- Work/stock.py
+++ Work/stock.py
@@ -1,8 +1,4 @@
 class Stock:
-    name=typedproperty('name',str)
-    shares=typedproperty('shares',int)
-    price=typedproperty('price',float)
-    
     __slots__=('name','_shares','price')
     def __init__ (self,name,shares,price):
         self.name=name
