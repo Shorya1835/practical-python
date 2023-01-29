aac6040094155e35c468e20d4d563ddabad75904
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-29 16:48:44 +0530

    Update test_stock.py

--- Work/test_stock.py
+++ Work/test_stock.py
@@ -7,8 +7,20 @@ class TestStock(unittest.TestCase):
     self.assertEqual(s.name,'GOOG')
     self.assertEqual(s.shares,100)
     self.assertEqual(s.price,490.1)
-    self.assertEqual(s.cost(),s.price*s.shares)'
-    self.assert s.sell(2)
+    
+  def test_cost(self):
+    s=stock.Stock('GOOG',100,490.1)
+    self.assertEqual(s.cost(),49010.0)
+    
+  def test_create(self):
+    s=stock.Stock('GOOG',100,490.1)
+    s.sell(25)
+    self.assertEqual(s.shares,75)
+  
+  def test_bad_shares(self):
+    s=stock.Stock('GOOG',100,490.1)
+    with self.assertRaises(TypeError):
+      s.shares='100'
     
 if __name__=='__main__':
   unittest.main()
