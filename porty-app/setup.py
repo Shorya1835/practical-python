15db6be39c00af095232d4161b43f46861476cd3
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-29 19:24:41 +0530

    Create setup.py

---
+++ porty-app/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+  name="porty",
+  version="0.01",
+  author="shorya",
+  author_email="shoryasinghal1835@gmail.com",
+  description="Stock portfolio",
+  packages=setuptools.find_packages(),
+)
