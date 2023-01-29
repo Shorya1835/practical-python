65ca24b61e54403308a382ec3be909b73817ca88
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-29 16:38:38 +0530

    Update test_stock.py

--- Work/test_stock.py
+++ Work/test_stock.py
@@ -1,7 +1,7 @@
 import unittest
 import stock
 
-class TestStock(unitest.TestCase):
+class TestStock(unittest.TestCase):
   def test_create(self):
     s=stock.Stock('GOOG',100,490.1)
     self.assertEqual(s.name,'GOOG')
