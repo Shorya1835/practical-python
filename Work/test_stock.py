7077c6be23815a3cb5c921c223ecec40fe9d9d62
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2023-01-29 16:34:19 +0530

    Create test_stock.py

---
+++ Work/test_stock.py
@@ -0,0 +1,12 @@
+import unitest
+import stock
+
+class TestStock(unitest.TestCase):
+  def test_create(self):
+    s=stock.Stock('GOOG',100,490.1)
+    self.assertEqual(s.name,'GOOG')
+    self.assertEqual(s.shares,100)
+    self.assertEqual(s.price,490.1)
+    
+if __name__=='__main__':
+  unittest.main()
